2413edb53fd990430947f22dd2e38cdbc398a69e
luccasgois <luccasgois1@gmail.com>
2025-04-20 10:59:56 -0300

    Add hability to generate HTML based on list of pages and data to be displayed on the templates

--- chitchat-cap-2/handlers.go
+++ chitchat-cap-2/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 
@@ -9,13 +10,18 @@ import (
 
 // Lets create the handler function for the root route localhost:port/
 func index(w http.ResponseWriter, r *http.Request) {
-	files := []string{
-		"templates/layout.html",
-		"templates/navbar.html",
-		"templates/index.html"}
-	templates := template.Must(template.ParseFiles(files...))
 	threads, err := data.Threads()
 	if err == nil {
-		templates.ExecuteTemplate(w, "layout", threads)
+		generateHTML(w, threads, "layout", "navbar", "index")
 	}
+}
+
+// Generates the base HTML based on the given templates and data to be displayed
+func generateHTML(w http.ResponseWriter, data interface{}, filesName ...string) {
+	files := []string{}
+	for _, fileName := range filesName {
+		files = append(files, fmt.Sprintf("templates/%s.html", fileName))
+	}
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
 }
